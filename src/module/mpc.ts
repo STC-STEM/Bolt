8e7d6092907d746d5c27924ea1e62f4b18ba83de
LaoSparrow <laosparrow@outlook.com>
2023-03-07 16:46:15 +0800

    chore: change mpc reminder time to 15:00

--- src/module/mpc.ts
+++ src/module/mpc.ts
@@ -86,7 +86,7 @@ export class MPCModule extends ModuleBase {
             }
         }))
 
-        cron.schedule('30 17 * * 1-5', async () => {
+        cron.schedule('0 17 * * 1-5', async () => {
             try {
                 let registeredChats = await MPCCron.find()
                 for (let c of registeredChats) {
