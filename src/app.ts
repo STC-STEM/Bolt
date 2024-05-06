b2a483f8b393d30cc42bec87bedfba5d95b4b697
LaoSparrow <laosparrow@outlook.com>
2024-05-06 16:30:46 +0800

    fix: adopt temporary solution from WWebJS issue #2885

--- src/app.ts
+++ src/app.ts
@@ -32,6 +32,10 @@ const client = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: {
         args: ['--no-sandbox']
+    },
+    webVersionCache: {
+        remotePath: 'https://github.com/wppconnect-team/wa-version/raw/main/html/2.2412.54.html',
+        type: 'remote'
     }
 })
 
